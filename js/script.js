40f9d96770afbc558addc7764403ba1a750f8136
EvaXausa <xausa.eva@gmail.com>
2022-12-15 21:42:13 +0100

    form

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log("JS OK");
+/*
+Palidroma
+1- Chiedere all’utente di inserire una parola tramite un form
+2- Creare una funzione per capire se la parola inserita è palindroma
+*/
+const resultElement = document.getElementsById("text");
